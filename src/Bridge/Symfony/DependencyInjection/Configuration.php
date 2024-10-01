edfc522eacb813c21367584315453cfba12ed995
David Dale <david@rubberdesk.com>
2024-10-01 14:42:56 +0100

    Add TreeBuilder return type for sf7

--- src/Bridge/Symfony/DependencyInjection/Configuration.php
+++ src/Bridge/Symfony/DependencyInjection/Configuration.php
@@ -19,7 +19,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
  */
 class Configuration implements ConfigurationInterface
 {
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('translation_adapter_loco');
         // Keep compatibility with symfony/config < 4.2
