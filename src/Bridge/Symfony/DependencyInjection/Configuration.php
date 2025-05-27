ac3de6eafabcab8c130b81090aaed9702475d3b6
David Dale <david@rubberdesk.com>
2025-05-27 14:39:25 +0100

    Return type

--- src/Bridge/Symfony/DependencyInjection/Configuration.php
+++ src/Bridge/Symfony/DependencyInjection/Configuration.php
@@ -19,10 +19,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
  */
 class Configuration implements ConfigurationInterface
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('translation_adapter_loco');
         // Keep compatibility with symfony/config < 4.2
